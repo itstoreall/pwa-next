9c82b04bab2ea957af44c48f754292434680e55e
Serhii Stanislav <serhiistanislav@gmail.com>
2024-10-01 22:52:28 +0300

    ADD | @ducanh2912/next-pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
+import withPWAInit from '@ducanh2912/next-pwa';
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  cacheOnFrontendNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true
+    // runtimeCaching: []
+  }
+});
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
